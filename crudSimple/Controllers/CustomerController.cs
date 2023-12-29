475f7ce1d7e593bc8522655a8a2e2dc56296c0b4
Juan José Seguí Borja <seguipowers@gmail.com>
2023-12-29 01:32:48 +0100

    Agregar archivos de proyecto.

---
+++ crudSimple/Controllers/CustomerController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using crudSimple.Dtos;
+
+namespace crudSimple.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        //List of Customers
+        [HttpGet]
+        public async Task<List<CustomerDto>> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        // GET Customer
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CustomerDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<CustomerDto> GetCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+        // DELETE Customer
+        [HttpDelete("{id}")]
+        public async Task<bool> DeleteCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST Customer
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(CustomerDto))]
+
+        public async Task<CustomerDto> CreateCustomer(CreateCustomerDto customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        // PUT Customer
+        [HttpPut]
+        public async Task<CustomerDto> UpdateCustomer(CustomerDto customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+        
+    
+}
---
+++ crudSimple/Dtos/CustomerDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crudSimple.Dtos
+{
+    public class CustomerDto
+    {
+        [Required]
+        public long Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        public string? LastName { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")]
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string Address { get; set; }
+
+       
+    }
+}
